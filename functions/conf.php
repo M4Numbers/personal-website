12dcf39c884c635913ce2ab7e5b306d635f2110d
Matthew D. Ball <m.d.ball2@ncl.ac.uk>
2016-08-19 11:45:30 +0100

    Cleaned up the first config file

---
+++ functions/conf.php
@@ -0,0 +1,13 @@
+<?php
+
+define('DATAHOST', 'localhost');
+define('DATAPORT', 3306);
+define('DATAUSER', 'username');
+define('DATAPASS', 'password');
+define('DATABASE', 'database');
+
+define('GOOGAPPNAME', 'webapp');
+define('GOOGSECRET', 'GoogleSecretCode');
+define('GOOGCHANID', 'YoutubeChannelId');
+
+define('PASSWORD', 'AdminMD5Hash');
