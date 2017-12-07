8caa2a2ee269a6284720b4e13aa1660c547db9b5
Matthew D. Ball <matthew.ball@ibm.com>
2017-12-07 07:10:20 +0000

    Moved into NodeJS with Express, Nunjucks, and SCSS.

--- gulpfile.js
+++ gulpfile.js
@@ -1,76 +1,11 @@
 "use strict";
 
 const gulp = require("gulp");
-const babel = require("gulp-babel");
-const clean = require("gulp-clean");
-const sass = require("gulp-sass");
+const npath = require("path");
+const spawn = require("child_process").spawn;
 
-const config = {
-    paths: {
-        input: {
-            root: "./src/**/*.*",
-            server: "./src/server/**/*.*",
-            common: "./src/common/**/*.*",
-            client: "./src/client/**/*.*"
-        },
-        scripts: {
-            scss: "./src/**/*.scss",
-            js: "./src/**/*.js",
-            ts: "./src/**/*.ts"
-        },
-        output: {
-            root: "./public",
-            server: "./public/server",
-            common: "./public/common",
-            client: "./public/client"
-        }
-    }
-};
-
-gulp.task("clean:all", function () {
-    return gulp.src([config.paths.output.root], {read: false})
-        .pipe(clean());
+gulp.task("run", [], () => {
+  spawn("node", [npath.resolve(__dirname, "bin/www")], {stdio: "inherit"});
 });
 
-gulp.task("clean:client", function () {
-    return gulp.src([config.paths.output.client], {read: false})
-        .pipe(clean());
-});
-
-gulp.task("clean:server", function () {
-    return gulp.src([config.paths.output.common, config.paths.output.server], {read: false})
-        .pipe(clean());
-});
-
-gulp.task("move:server", ["clean:server"], function () {
-    return gulp.src([config.paths.directories.server, config.paths.directories.common], {base: "."})
-        .pipe(gulp.dest(config.paths.dist.root));
-});
-
-gulp.task("build:client", ["clean:client"], () => {
-    return gulp.src([config.paths.scripts.scss])
-        .pipe(sass())
-        .pipe(gulp.dest(config.paths.output.root));
-});
-
-gulp.task("build:server", ["clean:server", "move:server"], () => {
-    return gulp.src([config.paths.script.server])
-        .pipe(babel({
-            presets: ["latest"],
-            plugins: ["transform-async-to-generator"]
-        }))
-        .pipe(gulp.dest(config.paths.dist.server));
-});
-
-gulp.task("build:common", ["clean:server", "move:server"], () => {
-    return gulp.src([config.paths.script.common])
-        .pipe(babel({
-            presets: ["latest"],
-            plugins: ["transform-async-to-generator"]
-        }))
-        .pipe(gulp.dest(config.paths.dist.common));
-});
-
-gulp.task("build:all", ["build:server", "build:common", "build:client"]);
-
-gulp.task("default", [], () => {});
+gulp.task("default", ["run"], () => {});
