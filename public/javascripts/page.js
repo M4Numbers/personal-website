61316361d32e61484591309bcb62fe90429b091a
Matthew D. Ball <matthew.ball@ibm.com>
2017-12-10 15:03:56 +0000

    Moved the javascript about properly to resolve linting issues

---
+++ public/javascripts/page.js
@@ -0,0 +1,17 @@
+const toTheTop = () => {
+    $("html, body").animate({ scrollTop: 0}, "slow");
+};
+
+const goto = (id) => {
+    $("html, body").animate(
+        {
+            scrollTop: $(id).offset().top
+        }, "slow");
+};
+
+const toggleMenu = () => {
+    document.getElementById("showLeftPush").classList.toggle("active");
+    document.getElementById("showLeftPush").classList.toggle("cbs-spmenu-push-toleft");
+    document.body.classList.toggle("cbp-spmenu-push-toleft");
+    document.getElementById("cbp-spmenu-s1").classList.toggle("cbp-spmenu-open");
+};
