fba516706b7ba060ba13d6997fc2471d52f76023
Matthew D. Ball <matthew.ball@ibm.com>
2017-12-22 12:46:27 +0000

    Moved to njk as an extension properly.

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ let app = express();
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "nunjucks");
+app.set("view engine", "njk");
 
 // uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, "public", "images", "icons", "favicon.ico")));
