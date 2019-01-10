274ac2ef2f594b7611dc3bc325320ed07c3e7f9f
Matthew D. Ball <m4numbers@gmail.com>
2019-01-10 07:20:00 +0000

    build(npm): moved away from yarn and updated packages
    Upgraded to latest versions of gulp and express

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,11 @@
 "use strict";
 
 const gulp = require("gulp");
-const exec = require("child_process").exec;
 const babel = require("gulp-babel");
 const sass = require("gulp-sass");
 const tildeImporter = require("node-sass-tilde-importer");
 
-gulp.task("sass", function () {
+gulp.task("sass", () => {
     return gulp
         .src("stylesheets/**/*.scss")
         .pipe(sass({
@@ -15,7 +14,7 @@ gulp.task("sass", function () {
         .pipe(gulp.dest("public/css"));
 });
 
-gulp.task("babel", [], () => {
+gulp.task("babel", () => {
     return gulp.src("js/**/*.js")
         .pipe(babel({
             plugins: ["transform-runtime"],
@@ -24,23 +23,10 @@ gulp.task("babel", [], () => {
         .pipe(gulp.dest("./public/js"));
 });
 
-gulp.task("sass:watch", ["sass"], () => {
+gulp.task("build", gulp.series("sass", "babel"));
+
+gulp.task("sass:watch", gulp.series("sass", () => {
    gulp.watch("stylesheets/**/*.scss", ["sass"]);
-});
+}));
 
-gulp.task("run", ["babel", "sass:watch"], () => {
-    exec("npm run start", function(err, stdout, stderr) {
-        console.log(stdout);
-        console.log(stderr);
-    });
-});
-
-gulp.task("test:eslint", [], () => {
-    exec("npm run quality:eslint", function(err, stdout, stderr) {
-        console.log(stdout);
-        console.log(stderr);
-    });
-});
-
-gulp.task("default", ["run"], () => {
-});
+gulp.task("default", gulp.series("build"));
