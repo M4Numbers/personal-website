51883454e56d60c94a9a35ab1ba42ac5e6461d9a
Jayne Doe <j4numbers@gmail.com>
2019-10-13 16:45:20 +0100

    refactor(copyright): change block comments to line comments

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,9 @@ module.exports = {
   ...require('@j4numbers/eslint-base-config'),
 
   env: {
+    'es6': true,
+    'commonjs': true,
+    'node': true,
     'browser': true,
     'jquery': true,
   }
