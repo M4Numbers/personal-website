6e7edf602bf78776ad58e8c712c66d330d6a6027
Matthew D. Ball <m4numbers@gmail.com>
2019-10-15 10:04:53 +0100

    refactor(admin-blog): split out the admin blog handlers into files

---
+++ src/journey/admin/blog/view_one_blog.js
@@ -0,0 +1,62 @@
+// MIT License
+//
+// Copyright (c) 2019 Jayne Doe
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+const errors = require('restify-errors');
+
+const BlogHandler = require('../../../lib/BlogHandler');
+const blogHandlerInstance = BlogHandler.getHandler();
+const renderer = require('../../../lib/renderer').nunjucksRenderer();
+
+const viewOneBlog = async (req, res, next) => {
+  try {
+    res.log.info(`Searching for blog of id ${req.params.blogId}`);
+    const blog = await blogHandlerInstance.findBlog(req.params.blogId);
+    res.contentType = 'text/html';
+    res.header('content-type', 'text/html');
+    const page = renderer.render('pages/admin/blogs/admin_blog_view_single.njk', {
+      top_page: {
+        title:     'Administrator Toolkit',
+        tagline:   'All the functions that the administrator of the site has available to them',
+        fa_type:   'fas',
+        fa_choice: 'fa-toolbox',
+      },
+
+      content: {
+        blog,
+      },
+
+      head: {
+        title:            'J4Numbers',
+        description:      'Home to the wild things',
+        current_page:     'admin',
+        current_sub_page: 'blog-view',
+      },
+    });
+    res.send(200, page);
+    next();
+  } catch (e) {
+    req.log.warn(`Issue found when trying to find single blog :: ${e.message}`);
+    next(new errors.InternalServerError(e.message));
+  }
+};
+
+module.exports = viewOneBlog;
