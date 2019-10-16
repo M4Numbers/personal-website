7b5284d715995649d7bc784648332e39de7053f0
Matthew D. Ball <m4numbers@gmail.com>
2019-10-16 11:37:30 +0100

    refactor(admin-kinks): split out the admin kink handler into files

---
+++ src/journey/admin/kinks/edit_single_kink.js
@@ -0,0 +1,41 @@
+// MIT License
+//
+// Copyright (c) 2019 Jayne Doe
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+const KinkHandler = require('../../../lib/KinkHandler');
+const kinkHandlerInstance = KinkHandler.getHandler();
+
+const editSingleKink = async (req, res, next) => {
+  try {
+    await kinkHandlerInstance.updateExistingKink(
+      req.params.kinkId, req.body[ 'kink-name' ],
+      req.body[ 'kink-description' ], req.body[ 'kink-status' ],
+      req.body[ 'kink-experience' ], req.body[ 'kink-image' ],
+      req.body[ 'kink-tags' ].split(/, ?/u)
+    );
+    res.redirect(303, `/admin/fetishes/${req.params.kinkId}`, next);
+  } catch (e) {
+    req.log.warn(`Issue found when trying to edit kink :: ${e.message}`);
+    res.redirect(303, `/admin/fetishes/${req.params.kinkId}`, next);
+  }
+};
+
+module.exports = editSingleKink;
